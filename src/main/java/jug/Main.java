63cf0e728a320123869c82ff4a6e1407386ba949
David Gageot <david@gageot.net>
2014-09-08 11:17:49 +0200

    Questions

--- src/main/java/jug/Main.java
+++ src/main/java/jug/Main.java
@@ -1,9 +1,31 @@
 package jug;
 
 import net.codestory.http.WebServer;
+import net.codestory.http.templating.ModelAndView;
 
 public class Main {
-    public static void main(String[] args) {
-        new WebServer(routes -> routes.catchAll("Hello World")).start();
-    }
+  public static void main(String[] args) {
+    new Main().start();
+  }
+
+  private void start() {
+    new WebServer(routes -> {
+      routes.get("/color/blue", () -> {
+        blue();
+        return ModelAndView.of("pass");
+      });
+      routes.get("/color/yellow", () -> {
+        yellow();
+        return ModelAndView.of("die");
+      });
+    }).start();
+  }
+
+  private void blue() {
+    System.out.println("BLUE");
+  }
+
+  private void yellow() {
+    System.out.println("YELLOW");
+  }
 }
