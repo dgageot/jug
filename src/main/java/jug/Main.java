353e61ec521b572622aa09b4ca3e5dc4a360cdb9
David Gageot <david@gageot.net>
2014-09-08 12:50:17 +0200

    Show hostname

--- src/main/java/jug/Main.java
+++ src/main/java/jug/Main.java
@@ -1,7 +1,13 @@
 package jug;
 
+import com.github.jknack.handlebars.ValueResolver;
 import net.codestory.http.WebServer;
 import net.codestory.http.templating.ModelAndView;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
   public static void main(String[] args) {
@@ -9,16 +15,17 @@ public class Main {
   }
 
   private void start() {
-    new WebServer(routes -> {
-      routes.get("/color/blue", () -> {
-        blue();
-        return ModelAndView.of("pass");
-      });
-      routes.get("/color/yellow", () -> {
-        yellow();
-        return ModelAndView.of("die");
-      });
-    }).start();
+    new WebServer(routes -> routes
+        .addHandleBarsResolver(new HostResolver())
+        .get("/color/blue", () -> {
+          blue();
+          return ModelAndView.of("pass");
+        })
+        .get("/color/yellow", () -> {
+          yellow();
+          return ModelAndView.of("die");
+        })
+    ).start();
   }
 
   private void blue() {
@@ -27,5 +34,27 @@ public class Main {
 
   private void yellow() {
     System.out.println("YELLOW");
+  }
+
+  static class HostResolver implements ValueResolver {
+    private final String hostname = hostname();
+
+    @Override
+    public Object resolve(Object context, String name) {
+      return "host".equals(name) ? hostname : null;
+    }
+
+    @Override
+    public Set<Map.Entry<String, Object>> propertySet(Object context) {
+      return null;
+    }
+
+    private static String hostname() {
+      try {
+        return InetAddress.getLocalHost().getHostName();
+      } catch (UnknownHostException e) {
+        return "<UNKNOWN>";
+      }
+    }
   }
 }
