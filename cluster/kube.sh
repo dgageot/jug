d51c1218c292688284a2b52a9639ed6482f7795b
David Gageot <david@gageot.net>
2014-10-06 18:33:49 +0200

    fix command

--- cluster/kube.sh
+++ cluster/kube.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -ti --volumes-from gcloud-config -v $HOME/.kubernetes_auth:/root/.kubernetes_auth -v $(pwd)/jug.json:/jug.json dgageot/kube /kube/cluster/kubecfg.sh "$@"
+docker run --rm -ti --volumes-from gcloud-config -v $HOME/.kubernetes_auth:/root/.kubernetes_auth -v $(pwd)/jug.json:/jug.json dgageot/kube "$@"
