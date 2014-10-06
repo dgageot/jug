f7b1e5dc5c52c994c9540c7ebccd8285f4a038d8
David Gageot <david@gageot.net>
2014-10-06 18:33:43 +0200

    Cheat: Don't use kube's docker

--- cluster/init.sh
+++ cluster/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -ti --volumes-from gcloud-config -v $HOME/.kubernetes_auth:/root/.kubernetes_auth dgageot/kube /kube/hack/dev-build-and-up.sh
+~/src/kubernetes /kube/hack/dev-build-and-up.sh
--- cluster/teardown.sh
+++ cluster/teardown.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -ti --volumes-from gcloud-config -v $HOME/.kubernetes_auth:/root/.kubernetes_auth dgageot/kube /kube/cluster/kube-down.sh
+~/src/kubernetes /kube/cluster/kube-down.sh
